03cdd5442c0d5186a414a3a81fa6a5a2eeb09d6e
guilherme pereira <134010081+bucho-op@users.noreply.github.com>
2023-06-18 18:40:30 -0300

    Add files via upload

---
+++ edit.js
@@ -0,0 +1,19 @@
+function is_even(a) {
+    return a % 2 == 0;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a,b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
